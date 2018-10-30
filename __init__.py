3b62ebcede10457dd66ffcba1ade8d6b5331b95a
chao <zju.chaoli@gmail.com>
2018-10-30 14:36:25 +0800

    chapter 3 at 2:36pm on 10/30/2018 by Chao Li

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 #place holder for module chao_app
 
+
+#we can add the function and class being exported here
+#__all__=[...]
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 
 from flask import Flask
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
 from config import Config
 from forms import LoginForm
 
@@ -38,10 +38,12 @@ def index():
 
 
 #mapping "/login" to login web page
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
 	form = LoginForm()
-
+	if form.validate_on_submit():
+		flash('login requested for user {}, remember_me={}'.format(form.username.data,form.remember_me.data))
+		return redirect(url_for('index'))
 	return render_template('login.html',title = 'Sign In', form = form)
 
 
@@ -49,6 +51,8 @@ def login():
 #main entry point
 if __name__ == '__main__':
 	app.run()
+else:
+	print("debugging the main Module")
 
 
 
